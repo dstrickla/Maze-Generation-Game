252334485f7a085380b1b762641ef2c2ff3c2d46
dstrickla <DanielStricklandjr13@gmail.com>
2024-10-02 00:29:46 -0400

    Add maze_generator.py to codebase

---
+++ maze_generator.py
@@ -0,0 +1,12 @@
+"""
+This class represents the maze generation algorithms used to randomly construct 
+a maze to be rendered as a playable level. This class was constructed to allow 
+for easy integration in other projects requiring maze generation. 
+"""
+
+class MazeGenerator():
+
+    def __init__(self):
+        pass
+
+    
